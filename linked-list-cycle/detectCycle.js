a104b0608fdfc1666074d842e1252cd472c2964f
Yad Smood <y.s.inside@gmail.com>
2016-06-06 00:13:06 +0800

    add: linked-list-cycle

---
+++ linked-list-cycle/detectCycle.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    var slow, fast;
+
+    slow = fast = head;
+
+    while (fast) {
+        fast = fast.next;
+
+        if (!fast) return null;
+
+        fast = fast.next;
+        slow = slow.next;
+
+        if (slow === fast) break;
+    }
+
+    if (!fast) return null;
+
+    // This is the key step
+    slow = head
+
+    while (slow != fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+};
---
+++ linked-list-cycle/test.js
@@ -0,0 +1,53 @@
+var test = require('../test');
+
+module.exports = test(__dirname + '/detectCycle.js', function (it, detectCycle) {
+    it(function () {
+        it.eq(
+            detectCycle({
+                val: 1,
+                next: {
+                    val: 2
+                }
+            }),
+            null
+        );
+    });
+
+    it(function () {
+        var list = {
+            val: 1,
+            next: {
+                val: 2,
+            }
+        }
+
+        list.next.next = list;
+
+        it.eq(
+            detectCycle(list) === list,
+            true
+        );
+    });
+
+    it('tail connects to node index 1', function () {
+        var list = {
+            val: 1,
+            next: {
+                val: 2,
+                next: {
+                    val: 3,
+                    next: {
+                        val: 4,
+                    }
+                }
+            }
+        }
+
+        list.next.next.next.next = list.next;
+
+        it.eq(
+            detectCycle(list) === list.next,
+            true
+        );
+    });
+});
